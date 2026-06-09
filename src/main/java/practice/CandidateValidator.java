daf2cd0dbffe69fef0cf103c479ad7e42f587820
Con <con@MacBook-Air-Con.lan>
2026-06-09 14:17:06 +0200

    Solved stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_CANDIDATE_AGE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int LIVING_TIME_IN_COUNTRY = 10;
+
+    // string has following view: "2002-2015"
+    private static Integer timeLivingInCountry(String fromTo) {
+        String[] parts = fromTo.split("-");
+        if (parts.length == 2) {
+            return Integer.parseInt(parts[1]) - Integer.parseInt(parts[0]);
+        }
+
+        throw new IllegalArgumentException("Invalid from-to: " + fromTo);
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_CANDIDATE_AGE
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
+                && candidate.isAllowedToVote()
+                && timeLivingInCountry(candidate.getPeriodsInUkr())
+                >= LIVING_TIME_IN_COUNTRY;
+    }
 }
